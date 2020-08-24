1c3888c8d3b431bb766a507575d15b4d0795fd3f
deepakchandh <deepak.cmr@zohocorp.com>
2020-08-24 17:44:49 +0530

    added cross origin and database name changes

--- src/main/java/com/java/books/resource/BookResourceImpl.java
+++ src/main/java/com/java/books/resource/BookResourceImpl.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import com.java.books.repository.BookRepository;
 
 @RestController
 @RequestMapping("/books")
-//@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="http://localhost:3000")
 public class BookResourceImpl implements Resource<Book> {
 	
 	@Autowired
@@ -48,33 +49,6 @@ public class BookResourceImpl implements Resource<Book> {
 			return new ResponseEntity<Book>(HttpStatus.OK);
 		}
 		return new ResponseEntity<Book>(HttpStatus.NOT_FOUND);
-	}
-	
-	
-	/*@Override
-	public ResponseEntity<Collection<Book>> findAll() {
-		return new ResponseEntity<>(bookService.findAll(), HttpStatus.OK);
-	}
-
-	@Override
-	public ResponseEntity<Book> findById(Long id) {
-		return new ResponseEntity<>(bookService.findById(id), HttpStatus.OK);
-	}
-
-	@Override
-	public ResponseEntity<Book> save(Book book) {
-		return new ResponseEntity<>(bookService.save(book), HttpStatus.CREATED);
-	}
-
-	@Override
-	public ResponseEntity<Book> update(Book book) {
-		return new ResponseEntity<>(bookService.update(book), HttpStatus.OK);
-	}
-
-	@Override
-	public ResponseEntity<Book> deleteById(Long id) {
-		return new ResponseEntity<>(bookService.deleteById(id), HttpStatus.OK);
-	}*/
-	
+	}	
 
 }
